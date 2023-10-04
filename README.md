d9770dfb6d880f73fd25d4c69065d8de1e1e3b25
kakunasa <kakunasagwc@gmail.com>
2023-10-05 00:53:49 +0900

    Configuring the AWS SDK V2 for Go

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # GolangAwsS3Demo
+
+### Project Description
 Use GoLang to configure AWS SDK and control file upload, file download and file deletion in S3 bucket
+
+### Pre-requirements
++ Use AWS IAM roles generate access key ID and secret access key，then set corresponding configuration items in the .env file
++ reference: <https://aws.github.io/aws-sdk-go-v2/docs/>
+
+### Project Initialization
+```bash
+git clone https://github.com/kakunasa/GolangAwsS3Demo.git
+go mod init GolangAwsS3Demo
+go get github.com/joho/godotenv
+go get github.com/aws/aws-sdk-go-v2
+go mod tidy
+go run .
+```
---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/joho/godotenv"
+)
+
+func loadEnv() {
+	if err := godotenv.Load(); err != nil {
+		fmt.Println("Error loading .env file:", err)
+	}
+}
+
+func main() {
+	loadEnv()
+	sdkConfig, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		fmt.Println("Couldn't load default configuration. Have you set up your AWS account?")
+		fmt.Println(err)
+		return
+	}
+	s3Client := s3.NewFromConfig(sdkConfig)
+	count := 10
+	fmt.Printf("Let's list up to %v buckets for your account.\n", count)
+	result, err := s3Client.ListBuckets(context.TODO(), &s3.ListBucketsInput{})
+	if err != nil {
+		fmt.Printf("Couldn't list buckets for your account. Here's why: %v\n", err)
+		return
+	}
+	if len(result.Buckets) == 0 {
+		fmt.Println("You don't have any buckets!")
+	} else {
+		if count > len(result.Buckets) {
+			count = len(result.Buckets)
+		}
+		for _, bucket := range result.Buckets[:count] {
+			fmt.Printf("\t%v\n", *bucket.Name)
+		}
+	}
+}
